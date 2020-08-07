bdc41f9e96cdad3a34f31a2a6a25d8a11f4e1f30
Sharmilan <68677999+SharmBB@users.noreply.github.com>
2020-08-07 21:22:33 +0530

    Add files via upload

---
+++ UsingStrings/app/src/main/java/com/example/usingstrings/ActivityOne.java
@@ -0,0 +1,20 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityOne extends AppCompatActivity {
+
+    TextView txtView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+
+        txtView2 = findViewById(R.id.textView2);
+        txtView2.setText(R.string.Msg2);
+    }
+}
